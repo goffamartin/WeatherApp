2d59c8e5764f6e8627d4d71c4f95587b7e3421ff
Martin Goffa <goffamartin@gmail.com>
2021-11-20 10:27:21 +0100

    start

---
+++ WeatherApp/WeatherApp/API_Caller.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Net.Http.Headers;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WeatherDesktopApp
+{
+    public class API_Caller
+    {
+        public static async Task<API_Response> Get(string url, string authID = null)
+        {
+            using (var client = new HttpClient())
+            {
+                if (!string.IsNullOrWhiteSpace(authID))
+                    client.DefaultRequestHeaders.Authorization = new AuthenticationHeaderValue("Authorization", authID);
+
+                var request = await client.GetAsync(url);
+                if (request.IsSuccessStatusCode)
+                {
+                    return new API_Response { Response = await request.Content.ReadAsStringAsync() };
+                }
+                else
+                    return new API_Response { ErrorMessage = request.ReasonPhrase };
+            }
+        }
+    }
+
+    public class API_Response
+    {
+        public bool Successuful => ErrorMessage == null;
+        public string ErrorMessage { get; set; }
+        public string Response { get; set; }
+    }
+}
