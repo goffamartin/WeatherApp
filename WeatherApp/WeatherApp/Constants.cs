e0daa2faf3a29894f802208348f7961cc644f649
Martin Goffa <goffamartin@gmail.com>
2021-11-28 12:18:12 +0100

    Odstranění klíče

--- WeatherApp/WeatherApp/Constants.cs
+++ WeatherApp/WeatherApp/Constants.cs
@@ -6,7 +6,7 @@ namespace WeatherApp
 {
     internal class Constants
     {
-        public const string GoogleMapsKey = "AIzaSyAtS_ahApNcoB7BoJdDopnScf4CWySsp3I";
-        public const string WeatherApiKey = "f4cdb9a4d3badec1ff1423c4a5fba527";
+        public const string GoogleMapsKey = "";
+        public const string WeatherApiKey = "";
     }
 }
