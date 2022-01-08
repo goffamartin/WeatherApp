d6287b18cc127f7879ef536b440ade408110f337
Martin Goffa <goffamartin@gmail.com>
2022-01-08 14:00:42 +0100

    unit test

--- UnitTest/UnitTest1.cs
+++ UnitTest/UnitTest1.cs
@@ -1,4 +1,7 @@
 using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System.Threading.Tasks;
+using WeatherApp;
+using Newtonsoft.Json;
 
 namespace UnitTest
 {
@@ -6,8 +9,33 @@ namespace UnitTest
     public class UnitTest1
     {
         [TestMethod]
-        public void TestMethod1()
+        public async Task APICallTest()
         {
+            // Arrange
+            string latitude = "50";
+            string longitude = "50";
+            string units = "metric";
+            string ApiKey = "f4cdb9a4d3badec1ff1423c4a5fba527";
+            string url = $"https://api.openweathermap.org/data/2.5/onecall?lat={latitude}&lon={longitude}&exclude=minutely&units={units}&appid={ApiKey}";
+            // Act
+            API_Response response = await API_Caller.Get(url);
+            // Assert
+            Assert.IsTrue(response.Successuful);
+        }
+        [TestMethod]
+        public async Task DeserializeIntoObject()
+        {
+            // Arrange
+            string latitude = "50";
+            string longitude = "50";
+            string units = "metric";
+            string ApiKey = "f4cdb9a4d3badec1ff1423c4a5fba527";
+            string url = $"https://api.openweathermap.org/data/2.5/onecall?lat={latitude}&lon={longitude}&exclude=minutely&units={units}&appid={ApiKey}";
+            // Act
+            API_Response response = await API_Caller.Get(url);
+            var info = JsonConvert.DeserializeObject<Rootobject>(response.Response);
+            // Assert
+            Assert.IsNotNull(info);
         }
     }
 }
